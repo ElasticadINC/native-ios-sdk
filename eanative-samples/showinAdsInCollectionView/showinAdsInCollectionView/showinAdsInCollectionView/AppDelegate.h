c2946be9675fa5529dd901a263c0b72ff63dc407
beniaminduca <beni@Beniamins-MacBook-Pro.local>
2016-01-20 10:41:05 +0200

    adding test images

---
+++ eanative-samples/showinAdsInCollectionView/showinAdsInCollectionView/showinAdsInCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  showinAdsInCollectionView
+//
+//  Created by Beniamin Duca on 20/01/16.
+//  Copyright © 2016 ElasticAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
